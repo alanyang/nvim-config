1a728d9abc56bea9c16a4fbc2796831d9f815e57
AlanYang <alanyang54@gmail.com>
2024-12-26 22:37:26 +0800

    bootstrap

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,26 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    presets = "modern",
+  },
+  keys = {
+    {
+      "<leader>t",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Toggle terminal",
+    },
+    {
+      "<leader>m",
+      function()
+        require("which-key").show({ global = true })
+      end,
+      desc = "Multiple cursor",
+    },
+  },
+}
